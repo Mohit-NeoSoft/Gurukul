96106e9fdeea9ac305f4a37309e6de009fa14b6c
User <user@Users-MacBook-Pro-3.local>
2024-03-05 17:28:24 +0530

    new changes

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.app.Gurukul',
-  appName: 'Demo',
+  appName: 'Gurukul',
   webDir: 'www',
   server: {
     androidScheme: 'https'
